78169babdea4d4b8dd3d9959da6d036bdc4492b8
Alexander Weinbeck <alexander.weinbeck@students.fhnw.ch>
2017-11-24 13:02:25 +0100

    added WelcomeController

---
+++ app/src/main/java/eu/wise_iot/wanderlust/controllers/WelcomeController.java
@@ -0,0 +1,19 @@
+package eu.wise_iot.wanderlust.controllers;
+
+/**
+ * WelcomeController:
+ * Handles welcome fragment
+ * @author Alexander Weinbeck
+ * @license MIT
+ */
+
+public class WelcomeController {
+    /**
+     * returns welcome string
+     * @return String
+     */
+   public static String getGreeting(){
+        return "Welcome";
+    }
+
+}
