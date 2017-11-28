21f18a864f2cce8dee92cd0a2359ce7af3713fc9
Alexander Weinbeck <alexander.weinbeck@students.fhnw.ch>
2017-11-28 13:11:57 +0100

    added fragment tour search, xml row for listview and added adapter for listview still needs to be implemented

---
+++ app/src/main/java/eu/wise_iot/wanderlust/controllers/ToursController.java
@@ -0,0 +1,10 @@
+package eu.wise_iot.wanderlust.controllers;
+
+/**
+ * ToursController:
+ * handles the toursfragment and its in and output
+ * @author Alexander Weinbeck
+ * @license MIT
+ */
+public class ToursController {
+}
---
+++ app/src/main/java/eu/wise_iot/wanderlust/views/ToursFragment.java
@@ -0,0 +1,10 @@
+package eu.wise_iot.wanderlust.views;
+
+/**
+ * ToursFragment:
+ * represents the tours UI
+ * @author Alexander Weinbeck
+ * @license MIT
+ */
+public class ToursFragment {
+}
