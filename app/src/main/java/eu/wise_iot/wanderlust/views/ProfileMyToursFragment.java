daebcf08d9abf34364aa3ca4601302733c6a39fa
Baris <baris.demirci@students.fhnw.ch>
2017-11-28 16:11:29 +0100

    profile view created

--- app/src/main/java/eu/wise_iot/wanderlust/views/ProfileMyToursFragment.java
+++ app/src/main/java/eu/wise_iot/wanderlust/views/ProfileMyToursFragment.java
@@ -3,11 +3,13 @@ package eu.wise_iot.wanderlust.views;
 import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
+import android.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import eu.wise_iot.wanderlust.R;
 
@@ -17,17 +19,9 @@ import eu.wise_iot.wanderlust.R;
  * @license MIT
  */
 public class ProfileMyToursFragment extends Fragment {
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
+    private ListView listView;
 
-
-
-    private String mParam1;
-    private String mParam2;
-
-    private OnFragmentInteractionListener mListener;
 
     public ProfileMyToursFragment() {
         // Required empty public constructor
@@ -45,8 +39,6 @@ public class ProfileMyToursFragment extends Fragment {
     public static ProfileMyToursFragment newInstance(String param1, String param2) {
         ProfileMyToursFragment fragment = new ProfileMyToursFragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
         fragment.setArguments(args);
         return fragment;
     }
@@ -54,55 +46,24 @@ public class ProfileMyToursFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile_my_tours, container, false);
+        View view = inflater.inflate(R.layout.fragment_profile_my_tours, container, false);
+        listView = (ListView) view.findViewById(R.id.tourList);
+
+        return view;
     }
 
-
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
-        }
-    }
-
-    @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
-        if (context instanceof OnFragmentInteractionListener) {
-            mListener = (OnFragmentInteractionListener) context;
-        } else {
-            throw new RuntimeException(context.toString()
-                    + " must implement OnFragmentInteractionListener");
-        }
-    }
 
     @Override
     public void onDetach() {
         super.onDetach();
-        mListener = null;
+
     }
 
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
-    public interface OnFragmentInteractionListener {
 
-        void onFragmentInteraction(Uri uri);
-    }
 }
