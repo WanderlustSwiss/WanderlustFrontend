17e2c442f86f364c907a641a23fd73390de01613
Rüegsegger Tobias (s) <tobias.rueegsegger@students.fhnw.ch>
2017-11-30 15:43:51 +0100

    implemented resetPassword

--- app/src/main/java/eu/wise_iot/wanderlust/controllers/LoginController.java
+++ app/src/main/java/eu/wise_iot/wanderlust/controllers/LoginController.java
@@ -1,8 +1,11 @@
 package eu.wise_iot.wanderlust.controllers;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+
+import javax.annotation.Nullable;
 
 import eu.wise_iot.wanderlust.models.DatabaseModel.LoginUser;
 import eu.wise_iot.wanderlust.models.DatabaseModel.User;
@@ -10,6 +13,9 @@ import eu.wise_iot.wanderlust.services.LoginService;
 import eu.wise_iot.wanderlust.services.ServiceGenerator;
 import eu.wise_iot.wanderlust.views.LoginFragment;
 import okhttp3.Headers;
+import okhttp3.MediaType;
+import okhttp3.RequestBody;
+import okio.BufferedSink;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
@@ -17,32 +23,62 @@ import retrofit2.Response;
 public class LoginController {
     private LoginFragment loginFragment;
 
-    public LoginController(LoginFragment fragment){
+    public LoginController(LoginFragment fragment) {
         this.loginFragment = fragment;
     }
 
-    public void logIn(LoginUser user, final FragmentHandler handler){
+    public void logIn(LoginUser user, final FragmentHandler handler) {
 
         LoginService service = ServiceGenerator.createService(LoginService.class);
         Call<User> call = service.basicLogin(user);
         call.enqueue(new Callback<User>() {
             @Override
             public void onResponse(Call<User> call, Response<User> response) {
-                if(response.isSuccessful()){
+                if (response.isSuccessful()) {
                     Headers headerResponse = response.headers();
                     Map<String, List<String>> headerMapList = headerResponse.toMultimap();
                     LoginUser.setCookies((ArrayList<String>) headerMapList.get("Set-Cookie"));
-                    handler.onResponse(new Event(EventType.getTypeByCode(response.code()),response.body()));
-                } else{
-                    handler.onResponse(new Event(EventType.getTypeByCode(response.code()),null));
+                    handler.onResponse(new Event(EventType.getTypeByCode(response.code()), response.body()));
+                } else {
+                    handler.onResponse(new Event(EventType.getTypeByCode(response.code()), null));
                 }
             }
 
             @Override
             public void onFailure(Call<User> call, Throwable t) {
-                handler.onResponse(new Event(EventType.NETWORK_ERROR,null));
+                handler.onResponse(new Event(EventType.NETWORK_ERROR, null));
             }
         });
     }
 
+    public void resetPassword(String email, final FragmentHandler handler) {
+
+        LoginService service = ServiceGenerator.createService(LoginService.class);
+        Call<Void> call = service.resetPassword(new EmailBody(email));
+        call.enqueue(new Callback<Void>() {
+            @Override
+            public void onResponse(Call<Void> call, Response<Void> response) {
+                Event event = new Event(EventType.getTypeByCode(response.code()), null);
+                    handler.onResponse(event);
+            }
+
+            @Override
+            public void onFailure(Call<Void> call, Throwable t) {
+                handler.onResponse(new Event(EventType.NETWORK_ERROR, null));
+            }
+        });
+    }
+
+    public class EmailBody {
+        private String email;
+
+        public EmailBody(String email) {
+            this.email = email;
+        }
+
+        public String getEmail() {
+            return email;
+        }
+    }
 }
+
--- app/src/main/java/eu/wise_iot/wanderlust/services/LoginService.java
+++ app/src/main/java/eu/wise_iot/wanderlust/services/LoginService.java
@@ -1,7 +1,9 @@
 package eu.wise_iot.wanderlust.services;
 
+import eu.wise_iot.wanderlust.controllers.LoginController;
 import eu.wise_iot.wanderlust.models.DatabaseModel.LoginUser;
 import eu.wise_iot.wanderlust.models.DatabaseModel.User;
+import okhttp3.RequestBody;
 import retrofit2.Call;
 import retrofit2.http.Body;
 import retrofit2.http.GET;
@@ -10,6 +12,9 @@ import retrofit2.http.POST;
 public interface LoginService {
     @POST("/auth/login")
     Call<User> basicLogin(@Body LoginUser user);
+
+    @POST("auth/forgottenpassword")
+    Call<Void> resetPassword(@Body LoginController.EmailBody body);
 
     @GET("/auth/authtest")
     Call<LoginUser> cookieTest();
