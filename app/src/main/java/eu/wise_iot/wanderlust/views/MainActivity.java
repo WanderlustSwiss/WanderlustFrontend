edd412e6fc63c4f3412881ad5a897e75bd140e11
Rüegsegger Tobias (s) <tobias.rueegsegger@students.fhnw.ch>
2017-12-03 16:47:20 +0100

    updated fake login

--- app/src/main/java/eu/wise_iot/wanderlust/views/MainActivity.java
+++ app/src/main/java/eu/wise_iot/wanderlust/views/MainActivity.java
@@ -89,7 +89,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
     private void fakeLogin(){
         LoginService loginService = ServiceGenerator.createService(LoginService.class);
-        LoginUser testUser = new LoginUser("zumsel321", "Ha11loW3lt");
+        LoginUser testUser = new LoginUser("testuser", "HalloW3lt");
         Call<LoginUser> call = loginService.basicLogin(testUser);
         call.enqueue(new Callback<LoginUser>() {
             @Override
