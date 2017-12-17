308e0c4ca13e24355e8a241f4a9f471baa6e2fc7
Simon Kaspar <simon.kaspar@studetns.fhnw.ch>
2017-12-17 11:23:20 +0100

    Fixed Server URL

--- app/src/main/java/eu/wise_iot/wanderlust/services/ServiceGenerator.java
+++ app/src/main/java/eu/wise_iot/wanderlust/services/ServiceGenerator.java
@@ -12,15 +12,10 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class ServiceGenerator {
 
-    //TODO change for production
     /*
      * Defines the URL for the backend communication
      */
-    // Staging Host
-    //public static final String API_BASE_URL = "http://86.119.40.34:8080/";
-
-    // Local Development Host (recommended)
-    public static final String API_BASE_URL = "http://10.0.2.2:1337";
+    public static final String API_BASE_URL = "https://www.cs.technik.fhnw.ch/wanderlust/";
 
     /**
      * Create service for a new backend request
