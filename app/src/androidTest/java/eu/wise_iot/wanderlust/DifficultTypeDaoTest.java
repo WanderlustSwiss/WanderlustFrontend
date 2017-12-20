9dabf32ceeea4ab0a49b11af6d7e66ed1a5b4384
Alexander Weinbeck <alexander.weinbeck@students.fhnw.ch>
2017-12-20 22:07:11 +0100

    added test for poi and difficultytype

--- app/src/androidTest/java/eu/wise_iot/wanderlust/DifficultTypeDaoTest.java
+++ app/src/androidTest/java/eu/wise_iot/wanderlust/DifficultTypeDaoTest.java
@@ -1,47 +1,104 @@
-//package eu.wise_iot.wanderlust;
+package eu.wise_iot.wanderlust;
+
+import android.content.Context;
+import android.support.test.InstrumentationRegistry;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.CountDownLatch;
+
+import static org.junit.Assert.*;
+
+import eu.wise_iot.wanderlust.models.DatabaseModel.DifficultyType;
+import eu.wise_iot.wanderlust.models.DatabaseModel.LoginUser;
+import eu.wise_iot.wanderlust.models.DatabaseModel.MyObjectBox;
+import eu.wise_iot.wanderlust.models.DatabaseModel.User;
+import eu.wise_iot.wanderlust.models.DatabaseObject.DifficultyTypeDao;
+import eu.wise_iot.wanderlust.services.LoginService;
+import eu.wise_iot.wanderlust.services.ServiceGenerator;
+import io.objectbox.Box;
+import io.objectbox.BoxStore;
+import okhttp3.Headers;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+
+/**
+ * Testing for DifficultyType DAO
+ * @author Alexander Weinbeck
+ * @license MIT
+ */
+
+public class DifficultTypeDaoTest {
 //
-//import android.content.Context;
-//import android.support.test.InstrumentationRegistry;
-//import android.support.test.runner.AndroidJUnit4;
-//
-//import org.junit.After;
-//import org.junit.Before;
-//import org.junit.Test;
-//import org.junit.runner.RunWith;
-//
-//import static org.junit.Assert.*;
-//
-//import eu.wise_iot.wanderlust.models.DatabaseModel.DifficultType;
-//import eu.wise_iot.wanderlust.models.DatabaseModel.DifficultType_;
-//import eu.wise_iot.wanderlust.models.DatabaseModel.MyObjectBox;
-//import eu.wise_iot.wanderlust.models.DatabaseObject.DifficultTypeDao;
-//import io.objectbox.Box;
-//import io.objectbox.BoxStore;
-//import io.objectbox.query.QueryBuilder;
-//
-//
-///**
-// * @author Rilind Gashi
-// */
-//
-//@RunWith(AndroidJUnit4.class)
-//public class DifficultTypeDaoTest {
-//
-//    BoxStore boxStore;
-//    Box<DifficultType> difficultyBox;
-//    QueryBuilder<DifficultType> difficultyQueryBuilder;
-//    DifficultType testDifficulty;
-//    DifficultTypeDao difficultTypeDao;
-//
-//    @Before
-//    public void setUpBefore(){
-//        Context appContext = InstrumentationRegistry.getTargetContext();
-//        boxStore = MyObjectBox.builder().androidContext(appContext).build();
-//        difficultTypeDao = new DifficultTypeDao(boxStore);
-//        difficultyBox = boxStore.boxFor(DifficultType.class);
-//        difficultyQueryBuilder = difficultyBox.query();
-//        testDifficulty = new DifficultType(0, "T1");
-//    }
+    BoxStore boxStore;
+
+    private Box<DifficultyType> difficultyTypeBox;
+
+    @Before
+    public void setUpBefore(){
+        Context appContext = InstrumentationRegistry.getTargetContext();
+        boxStore = MyObjectBox.builder().androidContext(appContext).build();
+        LoginService loginService = ServiceGenerator.createService(LoginService.class);
+
+        LoginUser testUser = new LoginUser("testuser", "Ha11loW4lt");
+
+        Call<User> call = loginService.basicLogin(testUser);
+        call.enqueue(new Callback<User>() {
+            @Override
+            public void onResponse(Call<User> call, Response<User> response) {
+                if (response.isSuccessful()) {
+                    Headers headerResponse = response.headers();
+                    //convert header to Map
+                    Map<String, List<String>> headerMapList = headerResponse.toMultimap();
+                    LoginUser.setCookies((ArrayList<String>) headerMapList.get("Set-Cookie"));
+
+                } else fail();
+            }
+            @Override
+            public void onFailure(Call<User> call, Throwable t) {
+                fail();
+            }
+        });
+    }
+    /**
+     * test to sync difficulty types with the database.
+     */
+    @Test
+    public void syncTest(){
+        try {
+            //generate test obj
+            DifficultyTypeDao dt = new DifficultyTypeDao();
+
+            CountDownLatch doneSignal = new CountDownLatch(1);
+            //perform sync
+            dt.sync();
+            //wait for async method
+            doneSignal.await();
+            //continue if arrived
+            //every DifficultyType of Server should be in Local database
+            for(DifficultyType dtIterate : difficultyTypeBox.getAll()){
+                //compare here with actual database data
+                //dtIterate.equals()
+            }
+
+        } catch (Exception e){
+            fail();
+        }
+        //make required assertion for properties
+//        assertEquals(userBox.get(1).getUser_id(),testUser.getUser_id());
+//        assertEquals(userBox.get(1).getAccountType(), testUser.getAccountType());
+//        assertEquals(userBox.get(1).isActive(),testUser.isActive());
+//        assertEquals(userBox.get(1).getEmail(), testUser.getEmail());
+//        assertEquals(userBox.get(1).getPassword(), testUser.getPassword());
+//        assertEquals(userBox.get(1).getNickname(), testUser.getNickname());
+    }
+
+
 //
 //    @Test
 //    public void createTest(){
@@ -161,4 +218,4 @@
 //        difficultyBox.removeAll();
 //        boxStore.close();
 //    }
-//}
+}
