b802aebf7577472e410dee1ff4702a602a5681c6
Alexander Weinbeck <alexander.weinbeck@students.fhnw.ch>
2017-12-21 01:21:00 +0100

    added profile dao tests but unknown reflection problems

--- app/src/androidTest/java/eu/wise_iot/wanderlust/ProfileDaoTest.java
+++ app/src/androidTest/java/eu/wise_iot/wanderlust/ProfileDaoTest.java
@@ -1,55 +1,244 @@
-//package eu.wise_iot.wanderlust;
-//
-//import android.content.Context;
-//import android.support.test.InstrumentationRegistry;
-//import android.support.test.runner.AndroidJUnit4;
-//
-//import org.junit.After;
-//import org.junit.Before;
-//import org.junit.Test;
-//import org.junit.runner.RunWith;
-//
-//import static org.junit.Assert.*;
-//
-//import eu.wise_iot.wanderlust.models.DatabaseModel.Profile;
-//import eu.wise_iot.wanderlust.models.DatabaseModel.Profile_;
-//import eu.wise_iot.wanderlust.models.DatabaseModel.MyObjectBox;
-//import eu.wise_iot.wanderlust.models.DatabaseObject.ProfileDao;
-//import io.objectbox.Box;
-//import io.objectbox.BoxStore;
-//import io.objectbox.query.QueryBuilder;
-//
-//
-///**
-// * @author Rilind Gashi
-// */
-//
-//@RunWith(AndroidJUnit4.class)
-//public class ProfileDaoTest {
-//
-//    BoxStore boxStore;
-//    Box<Profile> profileBox;
-//    QueryBuilder<Profile> profileQueryBuilder;
-//    Profile testProfile;
-//    ProfileDao profileDao;
-//
-//    @Before
-//    public void setUpBefore(){
-//        Context appContext = InstrumentationRegistry.getTargetContext();
-//        boxStore = MyObjectBox.builder().androidContext(appContext).build();
-//        profileDao = new ProfileDao(boxStore);
-//        profileBox = boxStore.boxFor(Profile.class);
-//        profileQueryBuilder = profileBox.query();
-//        testProfile = new Profile(0, "13.09.1995", 1, "picturePath");
-//    }
-//
+package eu.wise_iot.wanderlust;
+
+import android.content.Context;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.CountDownLatch;
+
+import static org.junit.Assert.*;
+
+import eu.wise_iot.wanderlust.controllers.ControllerEvent;
+import eu.wise_iot.wanderlust.controllers.FragmentHandler;
+import eu.wise_iot.wanderlust.models.DatabaseModel.LoginUser;
+import eu.wise_iot.wanderlust.models.DatabaseModel.Poi;
+import eu.wise_iot.wanderlust.models.DatabaseModel.Profile;
+import eu.wise_iot.wanderlust.models.DatabaseModel.Profile_;
+import eu.wise_iot.wanderlust.models.DatabaseModel.MyObjectBox;
+import eu.wise_iot.wanderlust.models.DatabaseModel.User;
+import eu.wise_iot.wanderlust.models.DatabaseObject.PoiDao;
+import eu.wise_iot.wanderlust.models.DatabaseObject.ProfileDao;
+import eu.wise_iot.wanderlust.services.LoginService;
+import eu.wise_iot.wanderlust.services.ServiceGenerator;
+import io.objectbox.Box;
+import io.objectbox.BoxStore;
+import io.objectbox.query.QueryBuilder;
+import okhttp3.Headers;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+
+/**
+ * Test class for Profile dao
+ * @author Rilind Gashi, Alexander Weinbeck
+ */
+
+@RunWith(AndroidJUnit4.class)
+public class ProfileDaoTest {
+
+    BoxStore boxStore;
+    Box<Profile> profileBox;
+    QueryBuilder<Profile> profileQueryBuilder;
+    Profile testProfile;
+    ProfileDao profileDao;
+
+    private final static int PROFILE_TEST_ID = 999;
+    private static final byte PROFILE_TEST_IMAGEID = 9;
+
+
+    @Before
+    public void setUpBefore() {
+
+        Context appContext = InstrumentationRegistry.getTargetContext();
+        boxStore = MyObjectBox.builder().androidContext(appContext).build();
+        LoginService loginService = ServiceGenerator.createService(LoginService.class);
+        LoginUser testUser = new LoginUser("testuser", "Ha11loW4lt");
+
+        Call<User> call = loginService.basicLogin(testUser);
+        call.enqueue(new Callback<User>() {
+            @Override
+            public void onResponse(Call<User> call, Response<User> response) {
+                if (response.isSuccessful()) {
+                    Headers headerResponse = response.headers();
+                    //convert header to Map
+                    Map<String, List<String>> headerMapList = headerResponse.toMultimap();
+                    LoginUser.setCookies((ArrayList<String>) headerMapList.get("Set-Cookie"));
+
+                } else fail();
+            }
+            @Override
+            public void onFailure(Call<User> call, Throwable t) {
+                fail();
+            }
+        });
+
+        //profileDao = new ProfileDao(boxStore, appContext);
+        profileBox = boxStore.boxFor(Profile.class);
+        profileQueryBuilder = profileBox.query();
+        //testProfile
+
+        //Profile(long internal_id, long profile_id, byte imageId, int score, String birthday, String language, long user, long difficulty)
+        testProfile = new Profile(99, 99, PROFILE_TEST_IMAGEID, 55, "01.01.1999", "EN", 999, 9);
+    }
+
+    @After
+    public void setUpAfter() {
+        //close boxstore
+        if(boxStore != null) boxStore.close();
+    }
+
+    /**
+     * test to update profile remote
+     */
+    @Test
+    public void updateTest(){
+        try {
+            CountDownLatch doneSignal = new CountDownLatch(1);
+
+            ProfileDao profileDao = new ProfileDao();
+            assertTrue(false);
+            profileDao.update(testProfile, new FragmentHandler() {
+                @Override
+                public void onResponse(ControllerEvent controllerEvent) {
+                    switch (controllerEvent.getType()){
+                        case OK:
+                            doneSignal.countDown();
+                            break;
+                        default:
+                            fail();
+                    }
+                }
+            });
+
+            //wait for async method
+            doneSignal.await();
+            //continue if arrived
+            //given testProfile from Remote should be in Local database
+            assertTrue(profileBox.getAll().contains(testProfile));
+
+        } catch (Exception e){
+            fail(e.getMessage());
+        }
+    }
+    /**
+     * test to retrieve profile remote
+     */
+    @Test
+    public void retrieveTest(){
+
+        try {
+            ProfileDao profileDao = new ProfileDao();
+            Profile testProfileUpdate = new Profile();
+
+            CountDownLatch doneSignal = new CountDownLatch(1);
+
+            profileDao.retrieve(testProfile, new FragmentHandler() {
+                @Override
+                public void onResponse(ControllerEvent controllerEvent) {
+                    switch (controllerEvent.getType()){
+                        case OK:
+                            doneSignal.countDown();
+                            break;
+                        default:
+                            fail();
+                    }
+                }
+            });
+            //wait for async method
+            doneSignal.await();
+            //continue if arrived
+            //given testProfile from Remote should be in Local database
+            assertTrue(profileBox.getAll().contains(testProfileUpdate));
+
+        } catch (Exception e){
+            fail(e.getMessage());
+        }
+    }
+    /**
+     * test to add image remote
+     */
+    @Test
+    public void addImageTest(){
+        try {
+            ProfileDao profileDao = new ProfileDao();
+
+            CountDownLatch doneSignal = new CountDownLatch(1);
+            profileDao.addImage(new File("testString"), testProfile, new FragmentHandler() {
+                @Override
+                public void onResponse(ControllerEvent controllerEvent) {
+                    switch (controllerEvent.getType()){
+                        case OK:
+                            doneSignal.countDown();
+                            break;
+                        default:
+                            fail();
+                    }
+                }
+            });
+
+            //wait for async method
+            doneSignal.await();
+            //continue if arrived
+            //given changed profile should be in Local database because of changes
+            assertTrue(profileBox.getAll().contains(testProfile));
+            //there should be an image now
+            assertTrue(profileBox.getAll().get(PROFILE_TEST_ID).getImageById(PROFILE_TEST_IMAGEID).equals(testProfile.getImageId()));
+        } catch (Exception e){
+            fail(e.getMessage());
+        }
+    }
+    /**
+     * test to delete image remote
+     */
+    @Test
+    public void deleteImageTest(){
+        try {
+            ProfileDao profileDao = new ProfileDao();
+
+            CountDownLatch doneSignal = new CountDownLatch(1);
+            profileDao.deleteImage(PROFILE_TEST_ID,PROFILE_TEST_IMAGEID, new FragmentHandler() {
+                @Override
+                public void onResponse(ControllerEvent controllerEvent) {
+                    switch (controllerEvent.getType()){
+                        case OK:
+                            doneSignal.countDown();
+                            break;
+                        default:
+                            fail();
+                    }
+                }
+            });
+
+            //wait for async method
+            doneSignal.await();
+            //continue if arrived
+            //given changed profile should not be in Local database because of changes
+            assertNull(profileBox.getAll().get(PROFILE_TEST_ID).getImageById(PROFILE_TEST_IMAGEID));
+        } catch (Exception e){
+            fail(e.getMessage());
+        }
+    }
+
+}
+
+
 //    @Test
-//    public void createTest(){
+//    public void createTest() {
 //        profileDao.create(testProfile);
 //        assertEquals("13.09.1995", profileQueryBuilder.equal(Profile_.birthdate, "13.09.1995")
 //                .build().findFirst().getBirthday());
 //    }
-//
+
 //    @Test
 //    public void updateTest(){
 //        profileBox.put(testProfile);
