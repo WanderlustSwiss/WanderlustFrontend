c168516e2b799d775fdc01c02c65f27af55019b3
Fabian Schwander <fabian.schwander@students.fhnw.ch>
2018-01-17 17:20:00 +0100

    added further ui tests

--- app/src/main/java/eu/wise_iot/wanderlust/services/ServiceGenerator.java
+++ app/src/main/java/eu/wise_iot/wanderlust/services/ServiceGenerator.java
@@ -21,7 +21,8 @@ public class ServiceGenerator {
     //public static final String API_BASE_URL = "http://86.119.40.34:8080/";
 
     // Local Development Host (recommended)
-    public static final String API_BASE_URL = "http://10.0.2.2:1337";
+//    public static final String API_BASE_URL = "http://10.0.2.2:1337";
+    public static final String API_BASE_URL = "http://192.168.1.143:1337";
 
     /**
      * Create service for a new backend request
