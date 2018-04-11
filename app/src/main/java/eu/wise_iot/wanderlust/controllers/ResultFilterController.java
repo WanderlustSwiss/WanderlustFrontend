6e467f1e1aab137222b936483c25fc12f1efc2df
Alexander Weinbeck <alexander.weinbeck@students.fhnw.ch>
2018-04-11 14:49:28 +0200

    merged with addtourregion and continued to link data

--- app/src/main/java/eu/wise_iot/wanderlust/controllers/ResultFilterController.java
+++ app/src/main/java/eu/wise_iot/wanderlust/controllers/ResultFilterController.java
@@ -7,10 +7,12 @@ import org.apache.commons.lang3.builder.Diff;
 import eu.wise_iot.wanderlust.models.DatabaseModel.DifficultyType_;
 import eu.wise_iot.wanderlust.models.DatabaseModel.Favorite;
 import eu.wise_iot.wanderlust.models.DatabaseModel.Favorite_;
+import eu.wise_iot.wanderlust.models.DatabaseModel.Region;
 import eu.wise_iot.wanderlust.models.DatabaseModel.Tour;
 import eu.wise_iot.wanderlust.models.DatabaseModel.Tour_;
 import eu.wise_iot.wanderlust.models.DatabaseObject.DifficultyTypeDao;
 import eu.wise_iot.wanderlust.models.DatabaseObject.FavoriteDao;
+import eu.wise_iot.wanderlust.models.DatabaseObject.RegionDao;
 import eu.wise_iot.wanderlust.models.DatabaseObject.UserTourDao;
 import io.objectbox.Property;
 
@@ -26,13 +28,13 @@ public class ResultFilterController {
     private static final String TAG = "TourOverviewController";
     private final UserTourDao userTourDao;
     private FavoriteDao favoriteDao;
+    private final RegionDao regionDao;
     private final DifficultyTypeDao difficultyType;
     private final ImageController imageController;
 
     public ResultFilterController(){
         userTourDao = UserTourDao.getInstance();
-        favoriteDao = FavoriteDao.getInstance();
-        favoriteDao = FavoriteDao.getInstance();
+        regionDao = RegionDao.getInstance();
         difficultyType = DifficultyTypeDao.getInstance();
         imageController = ImageController.getInstance();
     }
@@ -47,16 +49,16 @@ public class ResultFilterController {
         userTourDao.retrieveAllFiltered(handler, page, durationS, durationE, regionID, title, difficulties);
     }
     public int getRegionIdByString(String region) {
-        //userTourDao.findOne(Tour_)
+        regionDao.findOne(Region_.)
     }
     public String getDifficultiesByArray(boolean t1, boolean t2, boolean t3, boolean t4, boolean t5, boolean t6) {
         StringBuilder sb = new StringBuilder();
         if(t1) sb.append(difficultyType.findOne(DifficultyType_.level,1));
-        else if(t2) sb.append(difficultyType.findOne(DifficultyType_.level,2));
-        else if(t3) sb.append(difficultyType.findOne(DifficultyType_.level,3));
-        else if(t4) sb.append(difficultyType.findOne(DifficultyType_.level,4));
-        else if(t5) sb.append(difficultyType.findOne(DifficultyType_.level,5));
-        else if(t6) sb.append(difficultyType.findOne(DifficultyType_.level,6));
+        else if(t2) sb.append(difficultyType.findOne(DifficultyType_.level,2).getDifft_id());
+        else if(t3) sb.append(difficultyType.findOne(DifficultyType_.level,3).getDifft_id());
+        else if(t4) sb.append(difficultyType.findOne(DifficultyType_.level,4).getDifft_id());
+        else if(t5) sb.append(difficultyType.findOne(DifficultyType_.level,5).getDifft_id());
+        else if(t6) sb.append(difficultyType.findOne(DifficultyType_.level,6).getDifft_id());
         return sb.toString();
     }
 }
