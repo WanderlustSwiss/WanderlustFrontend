b2453eb3acc3c6eeca0b58881aa50c649456cfe6
Fabian Schwander <fabian.schwander@students.fhnw.ch>
2018-05-03 14:02:12 +0200

    added current year to disclaimer

--- app/src/main/java/eu/wise_iot/wanderlust/views/DisclaimerFragment.java
+++ app/src/main/java/eu/wise_iot/wanderlust/views/DisclaimerFragment.java
@@ -7,8 +7,12 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.squareup.picasso.Picasso;
+
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 
 import eu.wise_iot.wanderlust.R;
 
@@ -20,16 +24,31 @@ import eu.wise_iot.wanderlust.R;
  */
 public class DisclaimerFragment extends Fragment {
 
-    @Nullable
-    @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_disclaimer, container, false);
-    }
+    TextView authorsTextView;
 
     public static DisclaimerFragment newInstance() {
         Bundle args = new Bundle();
         DisclaimerFragment fragment = new DisclaimerFragment();
         fragment.setArguments(args);
         return fragment;
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_disclaimer, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        authorsTextView = (TextView) view.findViewById(R.id.authors_text_view);
+
+        Calendar calendar = new GregorianCalendar();
+        int year = calendar.get(Calendar.YEAR);
+        String authorsText = getActivity().getResources().getString(R.string.disclaimer_copy_wright);
+        authorsText += year + " ";
+        authorsText += getActivity().getResources().getString(R.string.disclaimer_authors);
+        authorsTextView.setText(authorsText);
     }
 }
