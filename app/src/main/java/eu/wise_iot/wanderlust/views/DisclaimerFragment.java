95cccb93b097d9bf5d67d16856f32401eb0fed62
Fabian Schwander <fabian.schwander@students.fhnw.ch>
2018-05-03 14:13:36 +0200

    added new camera icon

--- app/src/main/java/eu/wise_iot/wanderlust/views/DisclaimerFragment.java
+++ app/src/main/java/eu/wise_iot/wanderlust/views/DisclaimerFragment.java
@@ -6,10 +6,7 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
 import android.widget.TextView;
-
-import com.squareup.picasso.Picasso;
 
 import java.util.Calendar;
 import java.util.GregorianCalendar;
