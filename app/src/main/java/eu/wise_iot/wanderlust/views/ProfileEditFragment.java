7b777ba21ab791df6b8085cdf2a6f3e8447006d9
Fabian Schwander <fabian.schwander@students.fhnw.ch>
2018-05-19 17:37:20 +0200

    bugfixes, fonts adjusted and unused code removed

--- app/src/main/java/eu/wise_iot/wanderlust/views/ProfileEditFragment.java
+++ app/src/main/java/eu/wise_iot/wanderlust/views/ProfileEditFragment.java
@@ -7,7 +7,6 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.graphics.Color;
 import android.graphics.Rect;
 import android.net.Uri;
 import android.os.Bundle;
