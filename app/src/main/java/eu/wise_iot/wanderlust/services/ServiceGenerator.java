e24d9d0eda8e8ebe627a911086ff03c6e9a4da10
Alexander Weinbeck <alexander.weinbeck@students.fhnw.ch>
2018-05-19 14:39:10 +0200

    minor commenting on service generator

--- app/src/main/java/eu/wise_iot/wanderlust/services/ServiceGenerator.java
+++ app/src/main/java/eu/wise_iot/wanderlust/services/ServiceGenerator.java
@@ -9,7 +9,7 @@ import retrofit2.converter.gson.GsonConverterFactory;
 /**
  * ServiceGenerator handles all request for the backend database
  *
- * @author Tobias Rüegsegger
+ * @author Tobias Rüegsegger, Alexander Weinbeck
  * @license MIT
  */
 
@@ -18,12 +18,13 @@ public class ServiceGenerator {
     /*
      * Defines the URL for the backend communication
      */
+    //productive host
     //public static final String API_BASE_URL = "https://www.cs.technik.fhnw.ch/wanderlust/";
-    // Local Development Host (recommended)
-
+    //local development hosts (recommended)
     //public static final String API_BASE_URL = "http://10.0.2.2:1337";
     //public static final String API_BASE_URL = "http://192.168.56.1";
     //public static final String API_BASE_URL = "http://192.168.178.153:1337";
+    //build host
     public static final String API_BASE_URL = "http://86.119.40.34:8080";
 
     private static Retrofit service;
