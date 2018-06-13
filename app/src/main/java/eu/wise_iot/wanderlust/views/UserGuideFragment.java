2df90849a71a6962f55bd0102de40a21fabb969b
Fabian Schwander <fabian.schwander@students.fhnw.ch>
2018-06-13 16:06:47 +0200

    added checkbox to accept disclaimer

--- app/src/main/java/eu/wise_iot/wanderlust/views/UserGuideFragment.java
+++ app/src/main/java/eu/wise_iot/wanderlust/views/UserGuideFragment.java
@@ -1,14 +1,19 @@
 package eu.wise_iot.wanderlust.views;
 
 
+import android.app.Activity;
 import android.app.Fragment;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.Toast;
 
 import eu.wise_iot.wanderlust.R;
 import eu.wise_iot.wanderlust.constants.Constants;
@@ -22,6 +27,7 @@ import eu.wise_iot.wanderlust.constants.Constants;
 public class UserGuideFragment extends Fragment {
 
     private Button goToMapButton;
+    private CheckBox disclaimerAccepted;
 
 
     public static UserGuideFragment newInstance() {
@@ -39,21 +45,35 @@ public class UserGuideFragment extends Fragment {
                 R.layout.fragment_user_guide, container, false);
 
         goToMapButton = rootView.findViewById(R.id.btn_go_to_map);
+        disclaimerAccepted = rootView.findViewById(R.id.disclaimer_accepted_check_box);
+
         return rootView;
     }
 
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        goToMapButton.setBackgroundTintList(getActivity().getResources().getColorStateList(R.color.heading_icon_unselected));
+
+        disclaimerAccepted.setOnCheckedChangeListener((compoundButton, b) -> {
+            if (disclaimerAccepted.isChecked()) {
+                goToMapButton.setBackgroundTintList(getActivity().getResources().getColorStateList(R.color.primary_main));
+            } else {
+                goToMapButton.setBackgroundTintList(getActivity().getResources().getColorStateList(R.color.heading_icon_unselected));
+            }
+        });
 
         goToMapButton.setOnClickListener(v -> {
-
-            Fragment mapFragment = getFragmentManager().findFragmentByTag(Constants.MAP_FRAGMENT);
-            if (mapFragment == null) mapFragment = MapFragment.newInstance();
-            getFragmentManager().beginTransaction()
-                    .replace(R.id.content_frame, mapFragment, Constants.MAP_FRAGMENT)
-                    .commit();
-            ((AppCompatActivity) getActivity()).getSupportActionBar().show();
+            if (disclaimerAccepted.isChecked()) {
+                Fragment mapFragment = getFragmentManager().findFragmentByTag(Constants.MAP_FRAGMENT);
+                if (mapFragment == null) mapFragment = MapFragment.newInstance();
+                getFragmentManager().beginTransaction()
+                        .replace(R.id.content_frame, mapFragment, Constants.MAP_FRAGMENT)
+                        .commit();
+                ((AppCompatActivity) getActivity()).getSupportActionBar().show();
+            } else {
+                Toast.makeText(getActivity(), R.string.msg_accept_disclaimer, Toast.LENGTH_LONG).show();
+            }
         });
     }
 }
